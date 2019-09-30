d2fb6eaa4b63ae5b8b4f225fb8645585679d07de
gonzalomuga <gonzalo.garcia@unmsm.edu.pe>
2019-09-30 17:18:11 -0500

    version 2.0

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <conio.h>
+#include <sstream>
+
 
 using namespace std;
 
@@ -7,21 +9,27 @@ bool validaN(string entrada){
 	bool valida= true;
 	int tam = entrada.size();
 	for(int i=0;i<tam;i++){
-		if( entrada[i] <= 43 || entrada[i] >= 57 ){
-			valida = false;
-			break;
-		}
-	}
+		if( entrada[i] < 46 || entrada[i] > 57 ){
+                valida = false;
+                break;
+		} // limita el rango de entrada desde . a 9
+		if( entrada[i] == 47){
+			        valida = false;
+			        break;
+                }//excluye al slash de la lista limitada
+	}//itera para validar cada caracter ingresado
 	return valida;
-};
+}; //funcion que retorna el valor de valida como verdadero o falso
+
+
 
 
 int main() {
-	
+
 	string num1;
 	cout<< "Ingrese un numero: \n";
 	cin>> num1;
-	
+
 	while(!validaN(num1)){
 		cout<< "Oe no seas gil te dije numero ctmr \n";
 		cin.clear();
@@ -29,9 +37,27 @@ int main() {
 		cout<< "Ingrese un numero: \n";
 		cin>> num1;
 	}
-	
-	cout<< "congratulation no eres gil " ;
-	
+	string num2;
+	cout<< "Ingrese un numero: \n";
+	cin>> num2;
+
+	while(!validaN(num2)){
+		cout<< "Oe no seas gil te dije numero ctmr \n";
+		cin.clear();
+		cin.ignore();
+		cout<< "Ingrese un numero: \n";
+		cin>> num2;
+	}
+
+	float numv1= 0.0, numv2= 0.0;
+
+
+	istringstream (num1)>> numv1;
+	istringstream (num2)>> numv2;
+
+
+	cout<< "La suma es:" << numv1 + numv2 << endl ;
+
 	getch();
 	return 0;
 }
